525e76df4ddd167316b4a9dc4550f3507e511525
jio-kim <now20412041@gmail.com>
2022-03-08 11:40:52 +0900

    feat : frontend snippet

--- FE/pages/index.tsx
+++ FE/pages/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-
+/*
+className$
+여기에 개발 부분 설명 적기
+@author user$
+@version 1.0.0
+생성일 date$
+마지막 수정일 lastdate$
+*/
 const index = () => {
   return <div>폰트적용 확인</div>;
 };
