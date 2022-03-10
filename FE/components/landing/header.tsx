6bc8cbfa50f3809384133d17b6c1d39d98bcb1fa
changgun <pcg0527@naver.com>
2022-03-10 11:45:48 +0900

    chores: 코드정리

--- FE/components/landing/header.tsx
+++ FE/components/landing/header.tsx
@@ -5,14 +5,15 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logoBox}></div>
       <div className={styles.textBox}>
         <p className={styles.bigText}>향수를 해석하다</p>
         <p className={styles.smallText}>당신만의 향수를 찾아보세요</p>
       </div>
-      <Link href="/login">
-        <button className={styles.button}>나만의 향수 찾으러 가기</button>
-      </Link>
+      <div className={styles.buttonBox}>
+        <Link href="/login">
+          <button className={styles.button}>나만의 향수 찾으러 가기</button>
+        </Link>
+      </div>
     </header>
   );
 };
