ebb06009e60f642413fe070a814a0cfa03b7b3d4
wnsdl2494 <wnsdl2494@gmail.com>
2022-03-10 19:29:51 +0900

    feat #S06P22D104-14 : 소셜로그인 정보 DB에 저장

---
+++ BE/src/main/java/com/idle/common/oauth/dto/OAuthAttributes.java
@@ -0,0 +1,48 @@
+package com.ssafy.ej.config.auth.dto;
+
+import com.ssafy.ej.db.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .userId(email)
+                .userPw("social")
+                .userNickname(name)
+                .userEmail(email)
+                .build();
+    }
+}
--- BE/src/main/java/com/idle/common/test.java
+++
@@ -1,4 +0,0 @@
-package com.idle.common;
-
-public class test {
-}
