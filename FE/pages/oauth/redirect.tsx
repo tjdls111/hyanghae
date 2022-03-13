62024026ef2d36fb55715f02b7990311d7116184
changgun <pcg0527@naver.com>
2022-03-14 01:57:01 +0900

    chores : 주석 추가

--- FE/pages/oauth/redirect.tsx
+++ FE/pages/oauth/redirect.tsx
@@ -1,3 +1,13 @@
+/*
+구글로그인 redirect 페이지
+구글로그인 후 redirect되는 페이지이다.
+localStorage에 토큰을 저장하고, 홈으로 이동한다.
+@author user$
+@version 1.0.0
+생성일 date$
+마지막 수정일 lastdate$
+*/
+
 import React from "react";
 import { useRouter } from "next/router";
 
