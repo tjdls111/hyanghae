6bc413d66dc6af8eb4551d378e670c2dc58f41b8
changgun <pcg0527@naver.com>
2022-03-17 17:54:41 +0900

    feat #S06P22D104-137 : 로고 링크 추가

--- FE/components/UI/navigation.tsx
+++ FE/components/UI/navigation.tsx
@@ -4,7 +4,7 @@
 @author Jackson
 @version 1.0.0
 생성일 2022-03-14
-마지막 수정일 2022-03-15
+마지막 수정일 2022-03-17
 */
 
 import React, { useState } from "react";
@@ -14,6 +14,7 @@ import mainLogo from "../../public/logos/mainLogo.png";
 import MagnifyingGlass from "../../public/SVG/magnifying-glass.svg";
 import DeleteIcon from "../../public/SVG/circle-with-cross.svg";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const Navigation: React.FC = () => {
   const [keyword, setKeyword] = useState("");
@@ -23,36 +24,50 @@ const Navigation: React.FC = () => {
     setKeyword("");
   };
 
-  const keywordChangeHandler = function (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) {
-    setKeyword(e.target.value);
-  };
+  const keywordChangeHandler: React.ChangeEventHandler<HTMLInputElement> =
+    function (e) {
+      setKeyword(e.target.value);
+    };
 
-  const requestLoginHandler = function () {
-    router.push("/login");
-  };
+  const requestLoginHandler: React.MouseEventHandler<HTMLButtonElement> =
+    function () {
+      router.push("/login");
+    };
+
+  const searchSubmitHandler: React.FormEventHandler<HTMLFormElement> =
+    function (e) {
+      e.preventDefault();
+
+      // 검색 api
+    };
 
   return (
     <div className={styles.container}>
       <main className={styles.layout}>
-        <div className={styles.logoWrapper}>
-          <Image className={styles.logoImage} layout="fill" src={mainLogo} />
-        </div>
+        <Link href="/home">
+          <div className={styles.logoWrapper}>
+            <Image className={styles.logoImage} layout="fill" src={mainLogo} />
+          </div>
+        </Link>
         <nav className={styles.navContainer}>
           <ul className={styles.navigation}>
             <li className={styles.navItem}>향수추천</li>
             <li className={styles.navItem}>클래스</li>
           </ul>
         </nav>
-        <form className={styles.searchBar} action="">
+        <form
+          onSubmit={searchSubmitHandler}
+          className={styles.searchBar}
+          action=""
+        >
+          <MagnifyingGlass className={styles.searchIcon} />
           <input
             onChange={keywordChangeHandler}
             value={keyword}
             className={styles.searchInput}
             type="text"
           />
-          <MagnifyingGlass className={styles.searchIcon} />
+
           <DeleteIcon
             onClick={keywordDeleteHandler}
             className={styles.deleteIcon}
