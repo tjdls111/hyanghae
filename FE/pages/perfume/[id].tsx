0ead036c9f9f96ddc4749456b0fb9b879fc9c52e
Seoin <tjdls111@naver.com>
2022-03-22 17:39:50 +0900

    Fix : if문으로 404에러를 잡음

--- FE/pages/perfume/[id].tsx
+++ FE/pages/perfume/[id].tsx
@@ -45,53 +45,52 @@ interface PerfumeData {
 const Detail: NextPage = () => {
   const [data, setData] = useState({} as PerfumeData);
   const router = useRouter();
-
   useEffect(() => {
-    (async () => {
-      // api로 향수 상세 정보를 가져오기
-      apiPerfumeDetail(router.query.id as string)
-        .then((res) => {
-          console.log(res.data);
-          let myres: PerfumeResult = {
-            dayNight: "",
-            gender: "",
-            mood: "",
-            perfumeBrand: "",
-            perfumeCost: "",
-            perfumeDate: "",
-            perfumeId: "",
-            perfumeName: "",
-            perfumeScore: "",
-            perfumeUrl: "",
-            season: "",
-            tpo: "",
-          };
-          myres = res.data as PerfumeResult;
-          setData({
-            name: myres.perfumeName,
-            score: myres.perfumeScore,
-            price: myres.perfumeCost,
-            note: "",
-            imgPath:
-              "https://photo.akmall.com/image4/goods/78/58/60/94/78586094_M_350.jpg",
-            season: myres.season,
-            style: myres.tpo,
+    if (router.isReady) {
+      (async () => {
+        apiPerfumeDetail(router.query.id as string)
+          .then((res) => {
+            let myres: PerfumeResult = {
+              dayNight: "",
+              gender: "",
+              mood: "",
+              perfumeBrand: "",
+              perfumeCost: "",
+              perfumeDate: "",
+              perfumeId: "",
+              perfumeName: "",
+              perfumeScore: "",
+              perfumeUrl: "",
+              season: "",
+              tpo: "",
+            };
+            myres = res.data as PerfumeResult;
+            setData({
+              name: myres.perfumeName,
+              score: myres.perfumeScore,
+              price: myres.perfumeCost,
+              note: "",
+              imgPath:
+                "https://photo.akmall.com/image4/goods/78/58/60/94/78586094_M_350.jpg",
+              season: myres.season,
+              style: myres.tpo,
+            });
+          })
+          .catch((err) => {
+            console.log(err);
           });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    })();
+      })();
 
-    // 검색 api 로 검색 결과 가져오기 -> 결과가 있으면 연결되도록!
-    // apiShoppingSearch()
-    //   .then((res) => {
-    //     console.log(res);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-  }, [useRouter().isReady]);
+      // 이베이/ 네이버 등 쇼핑 검색 api 로 검색 결과 가져오기 -> 결과가 있으면 연결되도록!
+      // apiShoppingSearch()
+      //   .then((res) => {
+      //     console.log(res);
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
+    }
+  }, [router.isReady]);
 
   return (
     <>
