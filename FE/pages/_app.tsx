9bf50c4034c8cffb48fe32d7305c80b08d45ad46
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-03-22 14:53:01 +0900

    fix : jenkins build failure

--- FE/pages/_app.tsx
+++ FE/pages/_app.tsx
@@ -9,15 +9,9 @@ _app.tsx
 
 import "../styles/global.css";
 import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import store from "../store/index";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
