749c795bd05c5dad7ebf84ff7cd05742ba709339
wnsdl2494 <wnsdl2494@gmail.com>
2022-03-28 14:31:15 +0900

    fix #S06P22D104-177 #S06P22D104-175 : pagination 수정

--- BE/src/main/java/com/idle/db/repository/ReviewRepository.java
+++ BE/src/main/java/com/idle/db/repository/ReviewRepository.java
@@ -10,13 +10,12 @@
 package com.idle.db.repository;
 
 
-import antlr.collections.List;
 import com.idle.db.entity.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import java.util.Optional;
 
