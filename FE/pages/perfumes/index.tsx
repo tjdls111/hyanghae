6b001e5fcc93f903f6872de05a643d5754124e51
changgun <pcg0527@naver.com>
2022-04-05 09:41:42 +0900

    feat : Perfumes 페이지에 네비게이션 추가

--- FE/pages/perfumes/index.tsx
+++ FE/pages/perfumes/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { NextPage } from "next";
+import Navigation from "../../components/navigation/navigation";
 
-const PerfumesPage: Next = () => {
-  return <div>index</div>;
+const PerfumesPage: NextPage = () => {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
 };
 
 export default PerfumesPage;
