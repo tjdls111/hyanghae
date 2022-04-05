7586fb61e302653f886190780f992e7a005748e6
rhksvlf0704 <rhksvlf0704@gmail.com>
2022-04-05 13:50:00 +0900

    Fix : OAuth 주소 변경

--- BE/src/main/java/com/idle/common/oauth/OAuth2SuccessHandler.java
+++ BE/src/main/java/com/idle/common/oauth/OAuth2SuccessHandler.java
@@ -52,7 +52,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 //
 //        System.out.println(token);
 
-        response.sendRedirect("http://localhost:3001/oauth/redirect?token="+token);
+        response.sendRedirect("https://j6d104.p.ssafy.io/oauth/redirect?token="+token);
         var writer = response.getWriter();
 
         writer.flush();
