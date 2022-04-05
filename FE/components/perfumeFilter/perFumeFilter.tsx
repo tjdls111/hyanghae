03b582ea33d83df750b07b8f6f467d699de8c321
changgun <pcg0527@naver.com>
2022-04-05 10:26:12 +0900

    feat : container 컴포넌트

---
+++ FE/components/perfumeFilter/perFumeFilter.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./perfumeFilter.module.css";
+
+const PerfumeFilter: React.FC = () => {
+  return <div>test</div>;
+};
+
+export default PerfumeFilter;
--- FE/pages/perfumes/index.tsx
+++ FE/pages/perfumes/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { NextPage } from "next";
 import Navigation from "../../components/navigation/navigation";
+import Container from "../../components/ui/container/container";
+import PerfumeFilter from "../../components/perfumeFilter/perFumeFilter";
 
 const PerfumesPage: NextPage = () => {
   return (
     <>
       <Navigation />
+      <Container isTop={true}>
+        <PerfumeFilter />
+      </Container>
     </>
   );
 };
