f7c4db29676346a3c1aa64b1589d6a0d11a5d0f4
changgun <pcg0527@naver.com>
2022-04-06 17:57:50 +0900

    fix : productCard 반응형 쿼리 수정

--- FE/components/seasonalProduct/seasonalProduct.tsx
+++ FE/components/seasonalProduct/seasonalProduct.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./seasonalProducts.module.css";
 import ProductSlider from "../ui/slider/productSlider";
-import { DUMMY_DATA } from "./dummyData";
+import axios from "axios";
 
 const SeasonalProduct: React.FC = () => {
+  const [springPerfumes, setSpringPerfumes] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(process.env.NEXT_PUBLIC_BASE_URL + "/perfume/season/0")
+      .then((res) => setSpringPerfumes(res.data.perfumeList));
+  }, []);
+
   return (
     <section className={styles.container}>
       <ProductSlider
         header={"봄에 어울리는 향수"}
-        slideItems={DUMMY_DATA}
+        slideItems={springPerfumes}
       ></ProductSlider>
     </section>
   );
--- FE/components/ui/reviewCard/reviewCard.tsx
+++ FE/components/ui/reviewCard/reviewCard.tsx
@@ -3,34 +3,37 @@ import styles from "./reviewCard.module.css";
 import Image from "next/image";
 import Star from "../../../public/SVG/star_rate.svg";
 import { reviewType } from "perfume";
+import Link from "next/link";
 
 const ReviewCard: React.FC<{ review: reviewType }> = ({ review }) => {
   return (
-    <div className={styles.container}>
-      <section className={styles.productInfo}>
-        <div className={styles.productImgWrapper}>
-          <Image
-            className={styles.productImg}
-            src={review.imgUrl}
-            layout="fill"
-          />
-        </div>
-        <div className={styles.productText}>
-          <p className={styles.productName}>{review.perfumeName}</p>
-          <p className={styles.productBrand}>{review.perfumeBrand}</p>
-        </div>
-      </section>
-      <section className={styles.reviewInfo}>
-        <div className={styles.reviewHeader}>
-          <div className={styles.rating}>
-            <Star className={styles.ratingIcon} />
-            <p className={styles.ratingText}>{review.reviewScore}</p>
+    <Link href={`/perfume/${review.perfumeId}`}>
+      <div className={styles.container}>
+        <section className={styles.productInfo}>
+          <div className={styles.productImgWrapper}>
+            <Image
+              className={styles.productImg}
+              src={review.imgUrl}
+              layout="fill"
+            />
           </div>
-          <p className={styles.userNickname}>{review.userNickname}</p>
-        </div>
-        <div className={styles.reviewContent}>{review.reviewContent}</div>
-      </section>
-    </div>
+          <div className={styles.productText}>
+            <p className={styles.productName}>{review.perfumeName}</p>
+            <p className={styles.productBrand}>{review.perfumeBrand}</p>
+          </div>
+        </section>
+        <section className={styles.reviewInfo}>
+          <div className={styles.reviewHeader}>
+            <div className={styles.rating}>
+              <Star className={styles.ratingIcon} />
+              <p className={styles.ratingText}>{review.reviewScore}</p>
+            </div>
+            <p className={styles.userNickname}>{review.userNickname}</p>
+          </div>
+          <div className={styles.reviewContent}>{review.reviewContent}</div>
+        </section>
+      </div>
+    </Link>
   );
 };
 
