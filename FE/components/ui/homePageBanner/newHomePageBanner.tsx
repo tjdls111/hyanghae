6476b58a799241e6ae21f6b04fc26ecc54c20dfe
changgun <pcg0527@naver.com>
2022-04-06 16:29:16 +0900

    feat : 홈페이지 배너 링크 추가

--- FE/components/ui/homePageBanner/newHomePageBanner.tsx
+++ FE/components/ui/homePageBanner/newHomePageBanner.tsx
@@ -5,8 +5,19 @@ import "swiper/css";
 import "swiper/css/navigation";
 import styles from "./newHomePageBanner.module.css";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const NewHomePageBanner: React.FC = () => {
+  const router = useRouter();
+
+  const handleFirstButtonClick = function () {
+    router.push("/perfumes");
+  };
+
+  const handleSecondButtonClick = function () {
+    router.push("/survey");
+  };
+
   return (
     <>
       <Swiper
@@ -21,17 +32,18 @@ const NewHomePageBanner: React.FC = () => {
         <SwiperSlide>
           <div className={styles.container}>
             <div className={styles.imageWrapper}>
-              <h2 className={styles.title}>
-                사계절 모두 이용할 수 있는 전천후 향수
-              </h2>
+              <h2 className={styles.title}>어떤 향수를 써야할지 모르겠나요?</h2>
               <Image
                 className={styles.firstImage}
                 layout="fill"
                 src="/images/homePage-Banner/firstImage.webp"
                 alt="image of various perfumes"
               />
-              <button className={styles.descriptionBtn}>
-                인기 향수 보러 가기
+              <button
+                onClick={handleFirstButtonClick}
+                className={styles.descriptionBtn}
+              >
+                만족도 높은 향수 보러 가기
               </button>
             </div>
           </div>
@@ -55,7 +67,10 @@ const NewHomePageBanner: React.FC = () => {
                   나의 패션, 라이프스타일, 관심사에 따라 향수를 찾아보세요.
                   향수만큼 취향을 타는 것도 없어요!
                 </p>
-                <button className={styles.secondDescriptionBtn}>
+                <button
+                  onClick={handleSecondButtonClick}
+                  className={styles.secondDescriptionBtn}
+                >
                   향수 추천 받기
                 </button>
               </div>
