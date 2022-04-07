9d3e8a500bb623a1208aea1b05b0ec93dfe16261
changgun <pcg0527@naver.com>
2022-04-08 02:53:59 +0900

    fix : 랜딩 페이지 튜토리얼 섹션  텍스트 변경

--- FE/components/landing/tutorial.tsx
+++ FE/components/landing/tutorial.tsx
@@ -31,18 +31,17 @@ const Tutorial = () => {
             <p className={styles.stepNumber}>01</p>
             <p className={styles.stepHeading}>1000여 가지가 넘는 향수</p>
             <p className={styles.stepDescription}>
-              향해는 1000여 가지가 넘는 향수 중에서 당신에게 꼭 맞는 향수를
-              추천해 드립니다. 당신의 패션, 라이프스타일, 그리고 무드에 꼭 맞는
-              향수를 향해에서 찾아보세요.
+              향해는 1000여 가지가 넘는 향수의 데이터를 축적하고 있습니다. 수
+              많은 향수 중에서 여러분만의 향수를 찾아보세요
             </p>
           </div>
 
           <div className={styles.stepTextBox}>
             <p className={styles.stepNumber}>02</p>
-            <p className={styles.stepHeading}>전문 조향사</p>
+            <p className={styles.stepHeading}>추천 시스템</p>
             <p className={styles.stepDescription}>
-              오직 당신만을 위한 향기를 찾아드립니다. 향해에는 전문 조향사들이
-              상시 대기하고 있습니다. 뭐든지 물어보세요!
+              오직 당신만을 위한 향기를 찾아드립니다. 라이프스타일, 패션, 기존에
+              쓰고 있던 제품에 기반하여 꼭 맞는 향수를 추천해드립니다.
             </p>
           </div>
           <div className={styles.stepImgBox}>
@@ -64,7 +63,8 @@ const Tutorial = () => {
             <p className={styles.stepNumber}>03</p>
             <p className={styles.stepHeading}>리뷰와 평점 시스템</p>
             <p className={styles.stepDescription}>
-              향해는 신뢰도가 높은 리뷰와 평점 시스템을 제공합니다.
+              향해는 신뢰도가 높은 리뷰와 평점 시스템을 제공합니다. 단순히
+              향수의 데이터베이스가 아니라, 향수의 커뮤니티를 지향합니다.
             </p>
           </div>
         </div>
